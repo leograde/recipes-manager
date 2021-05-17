bbb4f7980fc323d18e558d99ccb33e08dd4bc5c5
Leonardo Grade <gradeleonardo@gmail.com>
2021-05-17 21:04:35 +0200

    fix: remove console.log

--- src/modules/recipe/domain/entities/recipe.factory.ts
+++ src/modules/recipe/domain/entities/recipe.factory.ts
@@ -6,7 +6,6 @@ export const RECIPE_FACTORY = 'RECIPE_FACTORY';
 @Injectable()
 export class RecipeFactory {
   createRecipe(recipeProps: IRecipeProps): Recipe {
-    console.log('hello');
     return Recipe.create(recipeProps);
   }
 }
